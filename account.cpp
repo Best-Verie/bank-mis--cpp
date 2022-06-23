01c3760731a6f16d9a2bffd8d7e01dad461a1f83
Best-Verie <irabestverie2016@gmail.com>
2022-06-23 12:12:30 +0200

    feat: deposit and withdraw

--- account.cpp
+++ account.cpp
@@ -50,10 +50,30 @@ void account::showAccount() const {
 int account::showAccountNo() const {
     return accountNo;
 }
+//void account::showAccountType() const {
+//    return type;
+//}
+
+void account::deposit(int amount) {
+    amount+= amount;
+}
+
+void account::withdraw(int amount) {
+    amount-= amount;
+}
+
+char account::showAccountType() const {
+    return type;
+}
+
+int account::showBalance() const {
+    return amount;
+}
 
 void save_account();
 void displayAccountDetails(int);
 void intro();
+void deposit_withdraw(int, int);
 
 
 int main(){
@@ -76,6 +96,17 @@ int main(){
         switch (ch) {
             case '1':
                 save_account();
+                break;
+
+            case '2':
+                cout<<"\n Enter the account number.:  ";
+                cin>>accountNumber;
+                deposit_withdraw(accountNumber, 1);
+                break;
+            case '3':
+                cout<<"\n Enter the account number.:  ";
+                cin>>accountNumber;
+                deposit_withdraw(accountNumber, 2);
                 break;
 
             case '4':
@@ -104,7 +135,7 @@ void intro(){
 void save_account(){
     account account;
     ofstream outFile;
-    outFile.open("account.txt", fstream::in | fstream::out | fstream::app);
+    outFile.open("account.dat", fstream::binary | fstream::out | fstream::app);
     account.createAccount();
     outFile.write(reinterpret_cast<const char *>(&account), sizeof(account));
     outFile.close();
@@ -114,7 +145,7 @@ void save_account(){
 void displayAccountDetails(int acNo){
     account account;
     bool flag = false;
-    ifstream inFile("account.txt");
+    ifstream inFile("account.dat");
 
     if(!inFile){
         cout<<"File could not be open !! Press any Key...";
@@ -131,6 +162,53 @@ void displayAccountDetails(int acNo){
     if(!flag){
         cout<<"\n\n Account number does not exist";
     }
+}
+
+
+void deposit_withdraw(int accNo, int option){
+    int amount;
+    bool found = false;
+    account account;
+    fstream File;
+    File.open("account.dat", ios::binary|ios::in|ios::out);
+    if(!File){
+        cout<<"File could not be open !! Press any Key...";
+        return;
+    }
+    while (!File.eof() && !found){
+        File.read(reinterpret_cast<char *>(&account), sizeof(account));
+        if(account.showAccountNo() == accNo){
+            account.showAccount();
+            if(option == 1){
+                cout<<"\n\n\tTO DEPOSITE AMOUNT ";
+                cout<<"\n\nEnter The amount to be deposited";
+                cin>>amount;
+                account.deposit(amount);
+
+            }
+            if(option == 2){
+                cout<<"\n\n\tTO WITHDRAW AMOUNT ";
+                cout<<"\n\nEnter The amount to be withdraw";
+                cin>>amount;
+                int balance = account.showBalance() - amount;
+                if((balance < 500 && account.showAccountType() == 'S') || (balance<1000 && account.showAccountType()=='C')){
+                    cout<<"Insufficient balance";
+                }
+                else{
+                    account.withdraw(amount);
+                }
+            }
+
+            int pos=(-1)*static_cast<int>(sizeof(account));
+            File.seekp(pos,ios::cur);
+            File.write(reinterpret_cast<char *> (&account), sizeof(account));
+            cout<<"\n\n\t Record Updated";
+            found=true;
+        }
+    }
+    File.close();
+    if(!found)
+        cout<<"\n\n Record Not Found ";
 }
 
 
